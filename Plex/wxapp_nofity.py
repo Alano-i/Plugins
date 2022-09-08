e19f21244a867997fa5e362e5ebcc4999bc1f35e
WadeChenn <821204616@qq.com>
2022-09-08 18:51:39 +0800

    [update] 添加自动下载依赖库

--- Plex/wxapp_nofity.py
+++ Plex/wxapp_nofity.py
@@ -3,6 +3,30 @@
 # Date: 2022/09/08
 # plex企业微信通知，基于tautulli通知规则编写 ，需要配合 tautulli 可正常使用。
 # pip3 install pyyaml
+#########################依赖库初始化###########################
+# 依赖库列表
+import os
+from importlib import import_module
+import sys
+import_list=[
+    'yaml',
+]
+# 判断依赖库是否安装,未安装则安装对应依赖库
+sourcestr = "https://pypi.tuna.tsinghua.edu.cn/simple/"  # 镜像源
+def GetPackage(PackageName):
+    comand = "pip install " + PackageName +" -i "+sourcestr
+    # 正在安装
+    print("------------------正在安装" + str(PackageName) + " ----------------------")
+    print(comand + "\n")
+    os.system(comand)
+for v in import_list:
+    try:
+        import_module(v)
+    except ImportError:
+        print("Not find "+v+" now install")
+        GetPackage(v)
+##############################################################
+
 import yaml
 import json, sys
 import os
