f9a2c6bef25303b8fd82cb60f24a57863231f651
Alano-i <68833595+Alano-i@users.noreply.github.com>
2023-02-10 12:05:44 +0800

    修复每日新闻未更新时依然获取的问题。每小时重试一次，直到获取到最新每日新闻。

--- MR-Plugins/daily_news/daily_news/command.py
+++ MR-Plugins/daily_news/daily_news/command.py
@@ -3,15 +3,17 @@ from mbot.openapi import mbot_api
 from mbot.core.params import ArgSchema, ArgType
 import logging
 from .daily_news import main
+server = mbot_api
 _LOGGER = logging.getLogger(__name__)
 
 @plugin.command(name='daily_news', title='每天60秒读懂世界', desc='获取每日新闻和天气并微信通知', icon='LocalPostOffice', run_in_background=True)
 def daily_news_echo(ctx: PluginCommandContext):
     try:
-        _LOGGER.info('「每天60秒读懂世界」开始获取每日新闻和天气')
-        main()
-        _LOGGER.info('「每天60秒读懂世界」获取每日新闻和天气完成,并已推送消息')
-        return PluginCommandResponse(True, f'「每天60秒读懂世界」获取每日新闻和天气完成,并已推送消息')
+        _LOGGER.info('「每天60秒读懂世界」手动运行，开始获取每日新闻和天气')
+        server.common.set_cache('is_get_news', 'daily_news', False)
+        if main():
+            _LOGGER.info('「每天60秒读懂世界」手动运行，获取每日新闻和天气完成,并已推送消息')
+        return PluginCommandResponse(True, f'「每天60秒读懂世界」手动运行，执行完成')
     except Exception as e:
         _LOGGER.error(f'出错了,{e}')
-        return PluginCommandResponse(False, f'「每天60秒读懂世界」获取每日新闻和天气失败，原因：{e}')
+        return PluginCommandResponse(False, f'「每天60秒读懂世界」手动运行，执行失败，原因：{e}')
