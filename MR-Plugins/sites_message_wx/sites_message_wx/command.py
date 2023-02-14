fd4e8a37c5f8abbd75bb21475cc38e8a46fdcd7f
Alano-i <68833595+Alano-i@users.noreply.github.com>
2023-02-14 13:32:07 +0800

    Update command.py

--- MR-Plugins/sites_message_wx/sites_message_wx/command.py
+++ MR-Plugins/sites_message_wx/sites_message_wx/command.py
@@ -5,13 +5,13 @@ import logging
 from .sites_message import main
 _LOGGER = logging.getLogger(__name__)
 
-@plugin.command(name='sites_message_wx', title='获取PT站内信和公告-微信通知', desc='点击执行站内信和公告获取并微信通知', icon='LocalPostOffice', run_in_background=True)
+@plugin.command(name='sites_message_wx', title='获取 PT 站消息', desc='点击获取 PT 站内信和公告并微信通知', icon='LocalPostOffice', run_in_background=True)
 def sites_message_echo(ctx: PluginCommandContext):
     try:
-        _LOGGER.info('开始获取站内信和公告')
+        _LOGGER.info('开始获取 PT 站内信和公告')
         main()
-        _LOGGER.info('站内信和公告获取完成')
-        return PluginCommandResponse(True, f'站内信和公告获取成功')
+        _LOGGER.info('PT 站内信和公告获取完成')
+        return PluginCommandResponse(True, f'PT 站内信和公告获取成功')
     except Exception as e:
-        _LOGGER.error(f'出错了,{e}')
-        return PluginCommandResponse(False, f'站内信和公告获取失败，原因：{e}')
+        _LOGGER.error(f'出错了,原因：{e}')
+        return PluginCommandResponse(False, f'PT 站内信和公告获取失败，原因：{e}')
