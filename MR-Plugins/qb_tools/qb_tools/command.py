fddfbbb763fe8d1d7f212072e6ccb24a90afca8d
Alano-i <68833595+Alano-i@users.noreply.github.com>
2023-04-03 19:51:09 +0800

    Update command.py

--- MR-Plugins/qb_tools/qb_tools/command.py
+++ MR-Plugins/qb_tools/qb_tools/command.py
@@ -12,11 +12,11 @@ plugins_name = '「QB 工具箱」'
 add_tag_config_list = [
     {
         "name": "✅ 开启",
-        "value": True
+        "value": 'on'
     },
     {
         "name": "📴 关闭",
-        "value": False
+        "value": 'off'
     }
 ]
 
@@ -24,8 +24,8 @@ add_tag_config_list = [
 def add_tag_m_echo(ctx: PluginCommandContext,
                 progress_path: ArgSchema(ArgType.String, '为以下保存路径的种子添加标签', '', default_value='', required=True),
                 add_tag_m_name: ArgSchema(ArgType.String, '添加的标签名称', '', default_value='', required=True),
-                add_tag_config: ArgSchema(ArgType.Enum, '是否启用，默认开启', '', enum_values=lambda: add_tag_config_list, default_value=True, multi_value=False, required=False)):
-    
+                add_tag_config: ArgSchema(ArgType.Enum, '是否启用，默认开启', '', enum_values=lambda: add_tag_config_list, default_value='on', multi_value=False, required=False)):
+    add_tag_config = bool(add_tag_config and add_tag_config.lower() != 'off')
     _LOGGER.info(f'{plugins_name}开始手动添加标签')
     _LOGGER.info(f"{plugins_name}将为保存路径为 ['{progress_path}'] 的种子添加 ['{add_tag_m_name}'] 标签")
     add_tag_m(add_tag_config, progress_path, add_tag_m_name)
