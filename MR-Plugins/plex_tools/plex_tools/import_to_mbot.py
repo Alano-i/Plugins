baa7f89f4052d7f9077fc522637dd5e48b4ecaf9
Alano-i <68833595+Alano-i@users.noreply.github.com>
2023-07-18 09:58:56 +0800

    Update import_to_mbot.py

--- MR-Plugins/plex_tools/plex_tools/import_to_mbot.py
+++ MR-Plugins/plex_tools/plex_tools/import_to_mbot.py
@@ -4,7 +4,7 @@ import time
 from plexapi.server import PlexServer
 
 plugins_name = '「PLEX 工具箱」'
-loger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 def import_config(config):
     global tmdb_api_key, mbot_url, mbot_api_key, plex_url, plex_token
     mbot_url = config.get('mbot_url','')
@@ -13,7 +13,7 @@ def import_config(config):
     plex_token = config.get('plex_token','')
 
 def push_sub(videos, media_type='Movie'):
-    loger.info(f"{plugins_name}正在查找本地媒体，请稍候...")
+    logger.info(f"{plugins_name}正在查找本地媒体，请稍候...")
     video_len=len(videos.all())
     for video,i in zip(videos.all(),range(video_len)):
         title = video.title
@@ -28,27 +28,27 @@ def push_sub(videos, media_type='Movie'):
                 url=f"{mbot_url}/api/subscribe/sub_tmdb?tmdb_id={tmdbid}&season_number={season_number}&media_type=TV&access_key={mbot_api_key}"
                 response = requests.get(url)
                 if response.status_code == 200:
-                    loger.info(f"{plugins_name}['{title}'] {i+1}/{video_len} 导入（提交订阅）成功")
+                    logger.info(f"{plugins_name}['{title}'] {i+1}/{video_len} 导入（提交订阅）成功")
                 else:
-                    loger.error(f"{plugins_name}['{title}'] {i+1}/{video_len} 导入（提交订阅）失败")
+                    logger.error(f"{plugins_name}['{title}'] {i+1}/{video_len} 导入（提交订阅）失败")
         else:
             url=f"{mbot_url}/api/subscribe/sub_tmdb?tmdb_id={tmdbid}&media_type=Movie&access_key={mbot_api_key}"
             response = requests.get(url)
             if response.status_code == 200:
-                loger.info(f"{plugins_name}['{title}'] {i+1}/{video_len} 导入（提交订阅）成功")
+                logger.info(f"{plugins_name}['{title}'] {i+1}/{video_len} 导入（提交订阅）成功")
             else:
-                loger.error(f"{plugins_name}['{title}'] {i+1}/{video_len} 导入（提交订阅）失败")
+                logger.error(f"{plugins_name}['{title}'] {i+1}/{video_len} 导入（提交订阅）失败")
         time.sleep(0.5)
 
 def push_sub_main(lib_name):
     try:
         plex = PlexServer(plex_url, plex_token)
     except Exception as e:
-        loger.error(f"{plugins_name}连接 Plex 服务器失败,原因：{e}")
+        logger.error(f"{plugins_name}连接 Plex 服务器失败,原因：{e}")
     try:
         videos = plex.library.section(lib_name)
         if videos:
             media_type=videos.TYPE
             push_sub(videos,media_type)
     except Exception as e:
-        loger.error(f"{plugins_name}获取媒体出现错误! 原因：{e}")
+        logger.error(f"{plugins_name}获取媒体出现错误! 原因：{e}")
