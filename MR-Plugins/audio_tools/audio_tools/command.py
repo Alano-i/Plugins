2dcdb6c055e528c558d9156091ad3a4bb56f8c9a
Alano-i <68833595+Alano-i@users.noreply.github.com>
2023-07-25 22:01:05 +0800

    Update command.py

--- MR-Plugins/audio_tools/audio_tools/command.py
+++ MR-Plugins/audio_tools/audio_tools/command.py
@@ -83,7 +83,7 @@ def audio_clip_m_echo(ctx: PluginCommandContext,
         if albums:
             album = albums_s[i]
         output_dir = output_dir or input_dir
-        audio_clip(input_dir,output_dir,cliped_folder,audio_start,audio_end,clip_configs,authors,year,narrators,series,album,art_album,use_filename,subject)
+        audio_clip(input_dir,output_dir,cliped_folder,audio_start,audio_end,clip_configs,authors,year,narrators,series,podcast_summary,album,art_album,use_filename,subject)
         time.sleep(5)
         try:
             audio_path = f"{output_dir}/{cliped_folder}"
@@ -139,12 +139,13 @@ def get_xml_url_echo(ctx: PluginCommandContext):
 def diy_abs_echo(ctx: PluginCommandContext,
                 folder_path: ArgSchema(ArgType.String, '输入路径', '/Media/音乐/有声书/', default_value='/Media/音乐/有声书/', required=True),
                 series: ArgSchema(ArgType.String, '系列：推荐填写书名', '', default_value=None, required=False),
+                podcast_summary: ArgSchema(ArgType.String, '介绍', '用于生成播客介绍', default_value='', required=False),
                 authors: ArgSchema(ArgType.String, '作者：推荐填写原著作家', '', default_value=None, required=False),
                 narrators: ArgSchema(ArgType.String, '演播者，多个示例：演播A,,演播B,,', '', default_value=None, required=False),
                 year: ArgSchema(ArgType.String, '发布年份', '', default_value=None, required=False)):
 
     logger.info(f"{plugins_name}任务\n开始运行 DIY 音频元数据\n输入路径：[{folder_path}]\n系列：['{series}']\n作者：['{authors}']\n演播者：['{narrators}']\n发布年份：['{year}']")
-    diy_abs(folder_path, series, authors, narrators, year)
+    diy_abs(folder_path, series, podcast_summary, authors, narrators, year)
     return PluginCommandResponse(True, f'DIY 音频元数据任务完成')
 
 @plugin.command(name='move_to_dir', title='整理有声书', desc='分配到子文件夹 1-100 101-200 201-300, 并添加元数据', icon='RuleFolder',run_in_background=True)
@@ -155,6 +156,7 @@ def move_to_dir_echo(ctx: PluginCommandContext,
                 use_filename_config: ArgSchema(ArgType.Enum, '文件名作为标题，默认开启', '', enum_values=lambda: use_filename_config_list, default_value='on', multi_value=False, required=False),
                 narrators: ArgSchema(ArgType.String, '演播者，多个示例：演播A,,演播B,,', '', default_value=None, required=False),
                 series: ArgSchema(ArgType.String, '系列：推荐填写书名', '', default_value=None, required=False),
+                podcast_summary: ArgSchema(ArgType.String, '介绍', '用于生成播客介绍', default_value='', required=False),
                 year: ArgSchema(ArgType.String, '发布年份', '', default_value=None, required=False),
                 album: ArgSchema(ArgType.String, '专辑：留空则自动按每100集划分', '', default_value=None, required=False),
                 art_album: ArgSchema(ArgType.String, '专辑艺术家：推荐填写书名', '', default_value=None, required=False),
@@ -166,8 +168,8 @@ def move_to_dir_echo(ctx: PluginCommandContext,
     if move_out_configs == 'move':
         move_out(output_dir)
     elif move_out_configs == 'add_and_move':
-        move_to_dir(output_dir,authors,year,narrators,series,album,art_album,move_out_configs,use_filename,subject)
+        move_to_dir(output_dir,authors,year,narrators,series,podcast_summary,album,art_album,move_out_configs,use_filename,subject)
         diy_abs(output_dir, series, authors, narrators, year)
     else:
-        all_add_tag(output_dir,authors,year,narrators,series,album,art_album,use_filename,subject,diy_cover)
+        all_add_tag(output_dir,authors,year,narrators,series, podcast_summary, album,art_album,use_filename,subject,diy_cover)
     return PluginCommandResponse(True, f'整理系列文件夹任务完成')
