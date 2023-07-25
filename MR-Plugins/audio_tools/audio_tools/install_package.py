699538a3ea9b0b7d3abbbf9faa28871872d8ba2c
Alano-i <68833595+Alano-i@users.noreply.github.com>
2023-07-25 16:35:20 +0800

    Update install_package.py

--- MR-Plugins/audio_tools/audio_tools/install_package.py
+++ MR-Plugins/audio_tools/audio_tools/install_package.py
@@ -3,13 +3,14 @@ import logging
 from importlib import import_module
 import time
 import os
-_LOGGER = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 plugins_name = '「有声书工具箱」'
 #########################依赖库初始化###########################
 # 依赖库列表
 import_list=[
     'mutagen',
-    'cn2an'
+    'cn2an',
+    'ffmpeg'
 ]
 # 判断依赖库是否安装,未安装则安装对应依赖库
 # sourcestr = "https://pypi.tuna.tsinghua.edu.cn/simple/"  # 镜像源
@@ -17,24 +18,26 @@ import_list=[
 sourcestr = "http://pypi.douban.com/simple/"  # 镜像源
 sourceurl = "pypi.douban.com"
 def GetPackage(PackageName):
+    if PackageName == 'ffmpeg':
+        PackageName == 'ffmpeg-python'
     comand = f"pip install {PackageName} -i {sourcestr} --trusted-host {sourceurl}"
     # 正在安装
-    _LOGGER.info(f"{plugins_name} - 安装依赖脚本, 正在安装依赖 + {str(PackageName)}")
-    _LOGGER.info(comand)
+    logger.info(f"{plugins_name} - 安装依赖脚本, 正在安装依赖 + {str(PackageName)}")
+    logger.info(comand)
     try:
         # os.system(comand)
         install_log = os.popen(comand)
-        # _LOGGER.error(f"「每天60秒读懂世界 - 安装依赖脚本」安装日志：{install_log}")
-        _LOGGER.info(f'{plugins_name} - 安装依赖脚本, 安装依赖日志如下:\n{install_log.read()}')
-        _LOGGER.warning(f'{plugins_name} - 安装依赖脚本, 如果下方报错：依赖库安装失败导致插件载入失败，请手动进入 MR 命令行安装，安装命令：pip install mutagen cn2an')
+        # logger.error(f"「每天60秒读懂世界 - 安装依赖脚本」安装日志：{install_log}")
+        logger.info(f'{plugins_name} - 安装依赖脚本, 安装依赖日志如下:\n{install_log.read()}')
+        logger.warning(f'{plugins_name} - 安装依赖脚本, 如果下方报错：依赖库安装失败导致插件载入失败，请手动进入 MR 命令行安装，安装命令：pip install mutagen cn2an')
         # os.system(f'{comand} | tee /data/plugins/daily_news/install.log')
     except Exception as e:
-        _LOGGER.error(f"{plugins_name} - 安装依赖脚本, 安装依赖库失败！原因：{e}")
+        logger.error(f"{plugins_name} - 安装依赖脚本, 安装依赖库失败！原因：{e}")
 for v in import_list:
     try:
         import_module(v)
     except ImportError:
-        _LOGGER.error(f"{plugins_name} - 安装依赖脚本, 没有找到需要的依赖库: {v} 现在开始安装！")
+        logger.error(f"{plugins_name} - 安装依赖脚本, 没有找到需要的依赖库: {v} 现在开始安装！")
 
         GetPackage(v)
 ##############################################################
