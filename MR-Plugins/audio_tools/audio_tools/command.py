24aee651c61414b1d61bbfaa636ba6dd6932bcb0
Alano-i <68833595+Alano-i@users.noreply.github.com>
2023-07-27 21:15:30 +0800

    Update command.py

--- MR-Plugins/audio_tools/audio_tools/command.py
+++ MR-Plugins/audio_tools/audio_tools/command.py
@@ -70,19 +70,19 @@ def cmd_config(config):
 @plugin.command(name='audio_clip_m', title='音频剪辑', desc='剪辑片头片尾，修改整理元数据', icon='LibraryMusic',run_in_background=True)
 def audio_clip_m_echo(ctx: PluginCommandContext,
                 input_dirs: ArgSchema(ArgType.String, last_time_input_dirs, '输入路径,末尾不带/，支持多条，一行一条/Media/有声书', default_value = last_time_input_dirs, required=False),
-                output_dir: ArgSchema(ArgType.String, '输出路径', '', default_value=None, required=False),
+                output_dir: ArgSchema(ArgType.String, '输出路径', '', default_value='', required=False),
                 cliped_folder: ArgSchema(ArgType.String, '已剪辑存放路径，默认：已剪辑', '', default_value='已剪辑', required=False),
                 audio_start: ArgSchema(ArgType.String, '剪辑开始时间', '默认：0，单位：秒', default_value='0', required=False),
                 audio_end: ArgSchema(ArgType.String, '剪辑结束倒数秒数', '默认：0，单位：秒', default_value='0', required=False),
                 clip_configs: ArgSchema(ArgType.Enum, '选择运行的操作，默认：剪辑、整理、添加元数据', '若仅剪辑，下方参数不生效。', enum_values=lambda: clip_config, default_value='clip_and_move', multi_value=False, required=False),
                 use_filename_config: ArgSchema(ArgType.Enum, '文件名作为标题，默认开启', '', enum_values=lambda: use_filename_config_list, default_value='on', multi_value=False, required=False),
-                authors: ArgSchema(ArgType.String, '作者：推荐填写原著作家', '', default_value=None, required=False),
-                narrators: ArgSchema(ArgType.String, '演播者，多个示例：演播A,,演播B,,', '', default_value=None, required=False),
-                series: ArgSchema(ArgType.String, '系列：推荐填写书名', '', default_value=None, required=False),
-                year: ArgSchema(ArgType.String, '发布年份', '', default_value=None, required=False),
-                albums: ArgSchema(ArgType.String, '专辑：留空则自动按每100集划分', '', default_value=None, required=False),
-                art_album: ArgSchema(ArgType.String, '专辑艺术家：推荐填写书名', '', default_value=None, required=False),
-                subject: ArgSchema(ArgType.String, '题材，例如：武侠，相声', '', default_value=None, required=False),
+                authors: ArgSchema(ArgType.String, '作者：推荐填写原著作家', '', default_value='', required=False),
+                narrators: ArgSchema(ArgType.String, '演播者，多个示例：演播A,,演播B,,', '', default_value='', required=False),
+                series: ArgSchema(ArgType.String, '系列：推荐填写书名', '', default_value='', required=False),
+                year: ArgSchema(ArgType.String, '发布年份', '', default_value='', required=False),
+                albums: ArgSchema(ArgType.String, '专辑：留空则自动按每100集划分', '', default_value='', required=False),
+                art_album: ArgSchema(ArgType.String, '专辑艺术家：推荐填写书名', '', default_value='', required=False),
+                subject: ArgSchema(ArgType.String, '题材，例如：武侠，相声', '', default_value='', required=False),
                 podcast_summary: ArgSchema(ArgType.String, '简介', '用于生成播客简介', default_value='', required=False)):
     if '影音视界' in input_dirs:
         input_dirs = f"/Media{input_dirs.split('影音视界')[1]}"
@@ -94,7 +94,7 @@ def audio_clip_m_echo(ctx: PluginCommandContext,
     input_dirs_s = input_dirs.split('\n')
     if albums:
         albums_s = albums.split('\n')
-    album = None
+    album = ''
     for i, input_dir in enumerate(input_dirs_s):
         if albums:
             album = albums_s[i]
@@ -125,9 +125,6 @@ def poscast_m_echo(ctx: PluginCommandContext,
     # audio_paths = /Media/有声书/三国
     # src_base_path = /Media/有声书
     state = False
-    # if src_base_path not in audio_paths:
-    #     audio_paths = f"/{src_base_path.strip('/')}/{audio_paths.strip('/')}"
-    # audio_paths = f"/{audio_paths.strip('/')}"
     if not book_title and not audio_paths:
         logger.info(f"{plugins_name}未设置书名和路径，请设置后重试")
         return
@@ -188,11 +185,11 @@ def get_xml_url_echo(ctx: PluginCommandContext,
 @plugin.command(name='diy_abs', title='修改metadata.abs', desc='修改 Audiobookshelf 元数据', icon='SwitchAccessShortcutAdd',run_in_background=True)
 def diy_abs_echo(ctx: PluginCommandContext,
                 folder_path: ArgSchema(ArgType.String, '输入路径', '/Media/有声书/', default_value='/Media/有声书/', required=True),
-                series: ArgSchema(ArgType.String, '系列：推荐填写书名', '', default_value=None, required=False),
+                series: ArgSchema(ArgType.String, '系列：推荐填写书名', '', default_value='', required=False),
                 podcast_summary: ArgSchema(ArgType.String, '简介', '用于生成播客简介', default_value='', required=False),
-                authors: ArgSchema(ArgType.String, '作者：推荐填写原著作家', '', default_value=None, required=False),
-                narrators: ArgSchema(ArgType.String, '演播者，多个示例：演播A,,演播B,,', '', default_value=None, required=False),
-                year: ArgSchema(ArgType.String, '发布年份', '', default_value=None, required=False)):
+                authors: ArgSchema(ArgType.String, '作者：推荐填写原著作家', '', default_value='', required=False),
+                narrators: ArgSchema(ArgType.String, '演播者，多个示例：演播A,,演播B,,', '', default_value='', required=False),
+                year: ArgSchema(ArgType.String, '发布年份', '', default_value='', required=False)):
 
     logger.info(f"{plugins_name}任务\n开始运行 DIY 音频元数据\n输入路径：[{folder_path}]\n系列：['{series}']\n作者：['{authors}']\n演播者：['{narrators}']\n发布年份：['{year}']")
     diy_abs(folder_path, series, podcast_summary, authors, narrators, year)
@@ -201,16 +198,16 @@ def diy_abs_echo(ctx: PluginCommandContext,
 @plugin.command(name='move_to_dir', title='整理有声书', desc='分配到子文件夹 1-100 101-200 201-300, 并添加元数据', icon='RuleFolder',run_in_background=True)
 def move_to_dir_echo(ctx: PluginCommandContext,
                 move_out_configs: ArgSchema(ArgType.Enum, '选择运行的操作，默认：🔖 DIY元数据', '', enum_values=lambda: move_out_config, default_value='diy', multi_value=False, required=False),
-                output_dir: ArgSchema(ArgType.String, '输入路径', '/Media/有声书/', default_value=None, required=True),
-                authors: ArgSchema(ArgType.String, '作者：推荐填写原著作家', '', default_value=None, required=False),
+                output_dir: ArgSchema(ArgType.String, '输入路径', '/Media/有声书/', default_value='', required=True),
+                authors: ArgSchema(ArgType.String, '作者：推荐填写原著作家', '', default_value='', required=False),
                 use_filename_config: ArgSchema(ArgType.Enum, '文件名作为标题，默认开启', '', enum_values=lambda: use_filename_config_list, default_value='on', multi_value=False, required=False),
-                narrators: ArgSchema(ArgType.String, '演播者，多个示例：演播A,,演播B,,', '', default_value=None, required=False),
-                series: ArgSchema(ArgType.String, '系列：推荐填写书名', '', default_value=None, required=False),
+                narrators: ArgSchema(ArgType.String, '演播者，多个示例：演播A,,演播B,,', '', default_value='', required=False),
+                series: ArgSchema(ArgType.String, '系列：推荐填写书名', '', default_value='', required=False),
                 podcast_summary: ArgSchema(ArgType.String, '简介', '用于生成播客简介', default_value='', required=False),
-                year: ArgSchema(ArgType.String, '发布年份', '', default_value=None, required=False),
-                album: ArgSchema(ArgType.String, '专辑：留空则自动按每100集划分', '', default_value=None, required=False),
-                art_album: ArgSchema(ArgType.String, '专辑艺术家：推荐填写书名', '', default_value=None, required=False),
-                subject: ArgSchema(ArgType.String, '题材，例如：武侠，相声', '', default_value=None, required=False),
+                year: ArgSchema(ArgType.String, '发布年份', '', default_value='', required=False),
+                album: ArgSchema(ArgType.String, '专辑：留空则自动按每100集划分', '', default_value='', required=False),
+                art_album: ArgSchema(ArgType.String, '专辑艺术家：推荐填写书名', '', default_value='', required=False),
+                subject: ArgSchema(ArgType.String, '题材，例如：武侠，相声', '', default_value='', required=False),
                 diy_cover_config: ArgSchema(ArgType.Enum, '修改封面，默认关闭', '需要输入文件夹下有cover.jpg', enum_values=lambda: use_filename_config_list, default_value='off', multi_value=False, required=False)):
     if '影音视界' in output_dir:
         output_dir = f"/Media{output_dir.split('影音视界')[1]}"
