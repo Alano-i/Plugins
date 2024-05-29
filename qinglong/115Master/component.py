ab7364f18c7270d749a7a91adc155cb8a8431cec
Alano-i <68833595+Alano-i@users.noreply.github.com>
2024-05-29 19:34:52 +0800

    Update component.py

--- qinglong/115Master/component.py
+++ qinglong/115Master/component.py
@@ -7,20 +7,6 @@ from notify import WeComNotify
 if push_notify:        
     wecom_notify = WeComNotify(corpid=corpid, corpsecret=corpsecret, agentid=agentid, proxy_api_url=proxy_api_url)
 
-# for i in range(3):
-#     try:
-#         client = P115Client(cookie)
-#         break
-#     except Exception as e:
-#         print(f'第 {i+1}/3 次构建115客户端发生异常，原因：{e}')
-#         if isinstance(e.args[0], dict):
-#             if not e.args[0].get('state', False) and e.args[0].get('errno', 9999) == 99:
-#                 print(f'115 cookie 已失效，请重新获取')
-#                 wecom_notify.send_news(title="115 cookie 已失效", message="无法检测到账号状态，请更换cookie", link_url="", pic_url=pic_url, touser=touser)
-#                 exit()
-#         time.sleep(3)
-#         continue
-
 cookies = cookies.split('\n')
 clients = []
 for cookie in cookies:
