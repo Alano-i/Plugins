06f6505cf620698c442786fbc54bd206551e5667
Alano-i <68833595+Alano-i@users.noreply.github.com>
2024-06-01 12:19:05 +0800

    Update sign.py

--- qinglong/115Master/sign.py
+++ qinglong/115Master/sign.py
@@ -7,23 +7,30 @@ class P115AutoSign:
         self.client = client
 
     def auto_sign(self):
-        @retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(3))
-        def sign():
-            print(f"开始执行签到...")
-            return self.client.user_points_sign_post()
-        try:
-            return sign()
-        except Exception as e:
-            print(f"签到时发生异常,(注意不能用web的cookie)，原因：{e}")
-            self.reason = e
-            return {}
+        result={}
+        for i in range(3):
+            try:
+                print(f"开始执行签到...")
+                result = self.client.user_points_sign_post()
+                break
+            except Exception as e:
+                print(f'第 {i+1}/3 次签到时发生异常,(注意不能用web的cookie进行签到)，原因：{e}')
+                time.sleep(3)
+                continue
+        return result
 
     def main(self):
         result = self.auto_sign()
+        title=''
         if result.get('state', False) and result.get('code',9999) == 0:
-            print("签到成功！")
+            if result.get('data', {}).get('first_require_sign', None) == 1:
+                print("签到成功！")
+                title="115今日签到成功"
+            else:
+                print("今天已经签过到了！")
+                title="115今日已经签过到了"
             if push_notify:
-                wecom_notify.send_news(title="115今日签到成功", message="", link_url="", pic_url=pic_url, touser=touser)
+                wecom_notify.send_news(title=title, message="", link_url="", pic_url=pic_url, touser=touser)
         else:
             print("签到失败！")
             if push_notify:
