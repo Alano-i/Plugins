d86458227ec737585fe48a698080650dbd319f7a
Alano-i <68833595+Alano-i@users.noreply.github.com>
2024-06-01 12:42:07 +0800

    Update sign.py

--- qinglong/115Master/sign.py
+++ qinglong/115Master/sign.py
@@ -8,6 +8,7 @@ class P115AutoSign:
 
     def auto_sign(self):
         result={}
+        self.reason = '未知，请查看日志！'
         for i in range(3):
             try:
                 print(f"开始执行签到...")
@@ -15,6 +16,7 @@ class P115AutoSign:
                 break
             except Exception as e:
                 print(f'第 {i+1}/3 次签到时发生异常,(注意不能用web的cookie进行签到)，原因：{e}')
+                self.reason = e
                 time.sleep(3)
                 continue
         return result
@@ -23,14 +25,16 @@ class P115AutoSign:
         result = self.auto_sign()
         title=''
         if result.get('state', False) and result.get('code',9999) == 0:
+            continuous_day = result.get('data', {}).get('continuous_day', None)
+            message = f"已连续签到 {continuous_day} 天" if continuous_day else ''
             if result.get('data', {}).get('first_require_sign', None) == 1:
-                print("签到成功！")
+                print(f"签到成功！{message}")
                 title="115今日签到成功"
             else:
-                print("今天已经签过到了！")
+                print(f"今天已经签过到了！{message}")
                 title="115今日已经签过到了"
             if push_notify:
-                wecom_notify.send_news(title=title, message="", link_url="", pic_url=pic_url, touser=touser)
+                wecom_notify.send_news(title=title, message=message, link_url="", pic_url=pic_url, touser=touser)
         else:
             print("签到失败！")
             if push_notify:
